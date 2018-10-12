9590e831ac0e4e0c3d7d6be9857b86ec79234f61
erez-code <41819488+erez-code@users.noreply.github.com>
2018-10-12 21:07:34 +0300

    Add files via upload

--- flying.js
+++ flying.js
@@ -2,13 +2,13 @@ var jetsOverTheSideCount = 0
 
 $(".planes").hover(function() {
   $(this).css('margin-left', function (index, curValue) {
-    if (parseInt(curValue, 10) < $("body").width() - 500) {
+    if (parseInt(curValue, 10) < $("body").width() - 200) {
     console.log($(this).attr('id'), "is at", parseInt(curValue, 10) +"px")
     return parseInt(curValue, 10) + Math.floor(50 + Math.random()*(70 + 1 - 50)) + 'px';}
     else {
 	jetsOverTheSideCount += 1;
 	$("#jetsOverTheSide").text(jetsOverTheSideCount);
-	return 0 + 'px';}
+	return "10px";}
 });
 
 });
