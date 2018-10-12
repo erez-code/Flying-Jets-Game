fd082398b5f9d788456771947ab3aa435c57449a
erez-code <41819488+erez-code@users.noreply.github.com>
2018-10-12 12:39:58 +0300

    Update flying.js

--- flying.js
+++ flying.js
@@ -2,7 +2,7 @@ $( "span" ).hover(function() {
   $(this).css('margin-left', function (index, curValue) {
     if (parseInt(curValue, 10) < $("body").width() - 500) {
     console.log(parseInt(curValue, 10))
-    return parseInt(curValue, 10) + 50 + 'px';}
+    return parseInt(curValue, 10) + Math.floor(50 + Math.random()*(70 + 1 - 50)) + 'px';}
     else {return 0 + 'px';}
 });
 
