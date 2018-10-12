83545bcb23181994973d15d2f59a69490c34e434
erez-code <41819488+erez-code@users.noreply.github.com>
2018-10-12 11:49:34 +0300

    Add files via upload

--- flying.js
+++ flying.js
@@ -1,8 +1,9 @@
 $( "span" ).hover(function() {
   $(this).css('margin-left', function (index, curValue) {
-    if (parseInt(curValue, 10) < 800) {
+    if (parseInt(curValue, 10) < $("body").width() - 500) {
     console.log(parseInt(curValue, 10))
-    return parseInt(curValue, 10) + 10 + 'px';}
+    return parseInt(curValue, 10) + 100 + 'px';}
+    else {return 0 + 'px';}
 });
 
 });
