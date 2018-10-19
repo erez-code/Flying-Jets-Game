00ead5bfb02b0314e7f1d442f5f91584920e959c
erez-code <41819488+erez-code@users.noreply.github.com>
2018-10-19 13:46:59 +0300

    Update flying.js

--- flying.js
+++ flying.js
@@ -2,7 +2,7 @@ var jetsOverTheSideCount = 0
 
 $(".planes").hover(function() {
   $(this).css('margin-left', function (index, curValue) {
-    if (parseInt(curValue, 10) < $("body").width() - 200) {
+    if (parseInt(curValue, 10) < $("body").width() - 300) {
     console.log($(this).attr('id'), "is at", parseInt(curValue, 10) +"px")
     return parseInt(curValue, 10) + Math.floor(50 + Math.random()*(70 + 1 - 50)) + 'px';}
     else {
